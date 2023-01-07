6b9f44255b6c2af80412ee2c0c464d87fab71293
Thiago Leão <thiagogorgulho@outlook.com>
2023-01-07 14:06:23 -0300

    Add axum framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use axum::{response::Html, routing::get, Json, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(json_handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
     println!("Hello, world!");
+}
+
+async fn json_handler() -> Json<&'static str> {
+    Json("")
 }
